2d0d2735ef166607ad049fc483398e899ef0826b
KFH <KFH@LAPTOP-TRN-140>
2024-04-16 13:31:11 +0300

    Add project files.

---
+++ WebApplication2/Controllers/CustomerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Customer> customers = new List<Customer>()
+            {
+                new Customer { CivilId = 123, Name = "faten", Phone = 1234 }
+            };
+            return View(customers); 
+        }
+    }
+}
---
+++ WebApplication2/Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication2.Models
+{
+    public class Customer
+    {
+        public int CivilId { get; set; }
+        public string Name { get; set; }
+        public int Phone { get; set; }
+    }
+}
